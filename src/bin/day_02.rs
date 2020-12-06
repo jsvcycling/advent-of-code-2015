a3d503b8eedcc03729ee7a97df5cce11ee10343f
Joshua Vega <jvega@jsvcycling.com>
2020-12-05 23:07:53 -0500

    [Day 02] Refactoring.

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -1,53 +1,39 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::fs::read_to_string;
 
-pub fn main() -> Result<()> {
-    let file = File::open("inputs/day_02.txt")?;
-    let mut reader = BufReader::new(file);
+pub fn main() {
+    let buf = read_to_string("inputs/day_02.txt").unwrap();
 
-    let mut total_paper = 0;
-    let mut total_ribbon = 0;
+    let results: (u32, u32) = buf
+        .lines()
+        .scan((0, 0), |state, l| {
+            // Parse the line into a vector of u32's.
+            let vals: Vec<u32> = l
+                .split('x')
+                .map(|v| v.trim().parse::<u32>().unwrap())
+                .collect();
 
-    loop {
-        let mut buffer = String::new();
-        let result = reader.read_line(&mut buffer);
-
-        if result.is_err() || result.unwrap() == 0 {
-            break;
-        }
-
-        // Parse the line into a vector of u32's.
-        let vals: Vec<u32> = buffer
-            .split('x')
-            .map(|v| v.trim().parse::<u32>().unwrap())
-            .collect();
-
-        // Wrapping paper
-        {
             // Compute the area of each side type, find the smallest side (as
             // slack) and double each side (front & back, left & right, top &
             // bottom).
             let sides = vec![vals[0] * vals[1], vals[0] * vals[2], vals[1] * vals[2]];
             let slack = sides.iter().min().unwrap();
-            let sides: u32 = sides.iter().map(|v| 2 * v).sum();
+            let sides = sides.iter().sum::<u32>();
 
-            total_paper += sides + slack;
-        }
+            state.0 += sides * 2 + slack;
 
-        // Ribbon
-        {
             // Find the longest side, the total sum of sides, and the amount of
             // ribbon needed for the now.
             let longest_side = vals.iter().max().unwrap();
-            let sides: u32 = vals.iter().sum();
-            let bow: u32 = vals.iter().product();
+            let sides = vals.iter().sum::<u32>();
+            let bow = vals.iter().product::<u32>();
 
-            total_ribbon += (sides - longest_side) * 2 + bow;
-        }
-    }
+            state.1 += (sides - longest_side) * 2 + bow;
 
-    println!("Part 1: {}", total_paper);
-    println!("Part 2: {}", total_ribbon);
+            Some(*state)
+        })
+        .last()
+        .unwrap();
 
-    Ok(())
+    println!("Part 1: {}", results.0);
+    println!("Part 2: {}", results.1);
 }
