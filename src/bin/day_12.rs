3e2556096776b6eb75631b6a36c48a1905cf82d1
Joshua Vega <jvega@jsvcycling.com>
2020-12-06 09:41:44 -0500

    [Day 12] Refactoring.

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,46 +1,36 @@
-use std::io::prelude::*;
-
-use std::fs::File;
-use std::io::{BufReader, Result};
+use std::fs::read_to_string;
 
 use json::JsonValue;
 
-fn traverse_and_sum(value: &JsonValue, skip_red: bool) -> i64 {
+fn compute(value: &JsonValue, skip_red: bool) -> i64 {
     match value {
         JsonValue::Number(_) => value.as_i64().unwrap(),
         JsonValue::Object(obj) => {
             let mut sum: i64 = 0;
 
-            for v in obj.iter() {
-                // If skip_red is enabled and one of the values in the object is
-                // "red" then immediately stop and return 0.
-                if skip_red && v.1.is_string() && v.1.as_str().unwrap() == "red".to_string() {
+            for (_, v) in obj.iter() {
+                // Skip the object if it contains the value "red".
+                if skip_red && v.is_string() && v.as_str().unwrap() == "red".to_string() {
                     return 0;
                 }
 
-                sum += traverse_and_sum(v.1, skip_red);
+                sum += compute(v, skip_red);
             }
 
             sum
         }
-        JsonValue::Array(vals) => vals.iter().map(|v| traverse_and_sum(v, skip_red)).sum(),
+        JsonValue::Array(vals) => vals.iter().fold(0, |acc, v| acc + compute(v, skip_red)),
         _ => 0,
     }
 }
 
-pub fn main() -> Result<()> {
-    let mut f = BufReader::new(File::open("inputs/day_12.txt")?);
-
-    let mut buf = String::new();
-    f.read_to_string(&mut buf)?;
-
+pub fn main() {
+    let buf = read_to_string("inputs/day_12.txt").unwrap();
     let root = json::parse(buf.as_str()).unwrap();
 
-    let part1 = traverse_and_sum(&root, false);
-    let part2 = traverse_and_sum(&root, true);
+    let part1 = compute(&root, false);
+    let part2 = compute(&root, true);
 
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
-
-    Ok(())
 }
